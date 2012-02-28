d6d6e72d29a634b2a5c863eb0d6fa923700e300a
John Hoo <huziyong@gmail.com>
2012-02-28 20:07:57 +0800

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace ExcelUtls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxColumn = new System.Windows.Forms.TextBox();
+            this.textBoxRow = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sheet = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cell = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBoxAvg = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.excel,
+            this.sheet,
+            this.cell});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(474, 430);
+            this.dataGridView1.TabIndex = 50;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // textBoxColumn
+            // 
+            this.textBoxColumn.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBoxColumn.Location = new System.Drawing.Point(69, 33);
+            this.textBoxColumn.MaxLength = 2;
+            this.textBoxColumn.Name = "textBoxColumn";
+            this.textBoxColumn.Size = new System.Drawing.Size(134, 21);
+            this.textBoxColumn.TabIndex = 0;
+            this.textBoxColumn.Text = "A";
+            this.textBoxColumn.TextChanged += new System.EventHandler(this.textBoxColumn_TextChanged);
+            this.textBoxColumn.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxColumn_KeyPress);
+            this.textBoxColumn.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBoxColumn_MouseDown);
+            this.textBoxColumn.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxColumn_Validating);
+            // 
+            // textBoxRow
+            // 
+            this.textBoxRow.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBoxRow.Location = new System.Drawing.Point(69, 71);
+            this.textBoxRow.MaxLength = 3;
+            this.textBoxRow.Name = "textBoxRow";
+            this.textBoxRow.Size = new System.Drawing.Size(134, 21);
+            this.textBoxRow.TabIndex = 1;
+            this.textBoxRow.Text = "1";
+            this.textBoxRow.TextChanged += new System.EventHandler(this.textBoxRow_TextChanged);
+            this.textBoxRow.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxRow_KeyPress);
+            this.textBoxRow.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBoxColumn_MouseDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 99;
+            this.label1.Text = "Col";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 98;
+            this.label2.Text = "Row";
+            // 
+            // textBoxSum
+            // 
+            this.textBoxSum.Location = new System.Drawing.Point(69, 110);
+            this.textBoxSum.Name = "textBoxSum";
+            this.textBoxSum.ReadOnly = true;
+            this.textBoxSum.Size = new System.Drawing.Size(134, 21);
+            this.textBoxSum.TabIndex = 4;
+            this.textBoxSum.Text = "0";
+            this.textBoxSum.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBoxColumn_MouseDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 97;
+            this.label3.Text = "Sum";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem,
+            this.exportToolStripMenuItem,
+            this.refreshToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(781, 24);
+            this.menuStrip1.TabIndex = 100;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.importToolStripMenuItem.Text = "Import(&I)";
+            this.importToolStripMenuItem.Click += new System.EventHandler(this.importToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.exportToolStripMenuItem.Text = "Export(&E)";
+            this.exportToolStripMenuItem.Click += new System.EventHandler(this.exportToolStripMenuItem_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.refreshToolStripMenuItem.Text = "Refresh(&R)";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxColumn);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxRow);
+            this.splitContainer1.Panel2.Controls.Add(this.label6);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxMin);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxMax);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxAvg);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxSum);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Size = new System.Drawing.Size(781, 484);
+            this.splitContainer1.SplitterDistance = 531;
+            this.splitContainer1.TabIndex = 101;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 486);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(781, 22);
+            this.statusStrip1.TabIndex = 102;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.aboutToolStripMenuItem.Text = "About(&A)";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // excel
+            // 
+            this.excel.HeaderText = "Excel";
+            this.excel.Name = "excel";
+            this.excel.ReadOnly = true;
+            this.excel.Width = 150;
+            // 
+            // sheet
+            // 
+            this.sheet.HeaderText = "Sheet";
+            this.sheet.Name = "sheet";
+            this.sheet.ReadOnly = true;
+            this.sheet.Width = 120;
+            // 
+            // cell
+            // 
+            this.cell.HeaderText = "Cell";
+            this.cell.Name = "cell";
+            this.cell.ReadOnly = true;
+            this.cell.Width = 180;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(57, 17);
+            this.toolStripStatusLabel1.Text = "Welcome";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.ActiveLinkColor = System.Drawing.Color.Maroon;
+            this.toolStripStatusLabel2.IsLink = true;
+            this.toolStripStatusLabel2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(116, 17);
+            this.toolStripStatusLabel2.Text = "Powered by @hutusi";
+            this.toolStripStatusLabel2.Click += new System.EventHandler(this.toolStripStatusLabel2_Click);
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(593, 17);
+            this.toolStripStatusLabel3.Spring = true;
+            // 
+            // textBoxAvg
+            // 
+            this.textBoxAvg.Location = new System.Drawing.Point(69, 146);
+            this.textBoxAvg.Name = "textBoxAvg";
+            this.textBoxAvg.ReadOnly = true;
+            this.textBoxAvg.Size = new System.Drawing.Size(134, 21);
+            this.textBoxAvg.TabIndex = 4;
+            this.textBoxAvg.Text = "0";
+            this.textBoxAvg.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBoxColumn_MouseDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "Avg";
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(69, 183);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.ReadOnly = true;
+            this.textBoxMax.Size = new System.Drawing.Size(134, 21);
+            this.textBoxMax.TabIndex = 4;
+            this.textBoxMax.Text = "0";
+            this.textBoxMax.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBoxColumn_MouseDown);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 12);
+            this.label5.TabIndex = 97;
+            this.label5.Text = "Max";
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(69, 221);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.ReadOnly = true;
+            this.textBoxMin.Size = new System.Drawing.Size(134, 21);
+            this.textBoxMin.TabIndex = 4;
+            this.textBoxMin.Text = "0";
+            this.textBoxMin.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBoxColumn_MouseDown);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 12);
+            this.label6.TabIndex = 97;
+            this.label6.Text = "Min";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 508);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Cell One";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxColumn;
+        private System.Windows.Forms.TextBox textBoxRow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn excel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sheet;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cell;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxMax;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAvg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using ClosedXML.Excel;
+
+namespace ExcelUtls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.dataGridView1.Dock = DockStyle.Fill;
+
+            // import dialog 
+            importDialog.Filter = "Excel File(*.xlsx)|*.xlsx";
+            importDialog.CheckFileExists = true;
+            importDialog.CheckPathExists = true;
+            importDialog.ValidateNames = true;
+            importDialog.Multiselect = true;
+
+            // export dialog
+            exportDialog.Filter = "Excel File(*.xlsx)|*.xlsx";
+
+            //
+            this.statusStrip1.Items[0].Text = "Welcome :)";
+        }
+
+        private string GetValueString(object obj)
+        {
+            return obj == null ? "" : obj.ToString();
+        }
+
+        private int ParseToInt(object obj)
+        {
+            try
+            {
+                return int.Parse(obj.ToString());
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private void textBoxColumn_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                TextBox tb = sender as TextBox;
+                if (tb != null)
+                {
+                    tb.SelectAll();
+                }
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                string dir = Directory.GetCurrentDirectory();
+                string file = dir + "\\" + dataGridView1.Rows[e.RowIndex].Cells["Excel"].Value.ToString();
+                try
+                {
+                    System.Diagnostics.Process.Start(file);
+                }
+                catch
+                {
+                    MessageBox.Show(file + "无法打开！");
+                }
+            }
+        }
+
+        private void Show(int sheetNo, string cellName)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns["sheet"].HeaderText = "Sheet (" + sheetNo.ToString() + ")";
+            dataGridView1.Columns["cell"].HeaderText = "Cell (" + cellName + ")";
+            foreach (KeyValuePair<string, XLWorkbook> wb in wbs)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                var ws = wb.Value.Worksheet(sheetNo);
+                var cell = ws.Cell(cellName);
+
+                dataGridView1.Rows.Add(new object[] { Path.GetFileName(wb.Key), ws.Name, cell.Value });
+            }
+        }
+
+        private void Calc()
+        {
+            string cellName = this.textBoxColumn.Text.Trim() + this.textBoxRow.Text.Trim();
+            Show(1, cellName);
+
+            List<int> vals = new List<int>();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                vals.Add(ParseToInt(row.Cells["cell"].Value));
+            }
+
+            this.textBoxSum.Text = vals.Sum().ToString();
+            this.textBoxAvg.Text = vals.Average().ToString();
+            this.textBoxMax.Text = vals.Max().ToString();
+            this.textBoxMin.Text = vals.Min().ToString();
+        }
+
+        private void ParseExcels()
+        {
+            foreach (string f in files)
+            {
+                wbs[f] = new XLWorkbook(f);
+            }
+            Calc();
+        }
+        
+        private void importToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (importDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = new List<string>(importDialog.FileNames);
+                ParseExcels();
+            }
+        }
+
+        private void exportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (exportDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var wb = new XLWorkbook();
+                var ws = wb.AddWorksheet("Sheet1");
+
+                for (int i = 0; i < dataGridView1.Columns.Count; ++i)
+                {
+                    ws.Cell(1, i + 1).Value = dataGridView1.Columns[i].HeaderText;
+                }
+
+                for (int i = 0; i < dataGridView1.Rows.Count; ++i)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; ++j)
+                    {
+                        ws.Cell(i + 2, j + 1).Value = GetValueString(dataGridView1.Rows[i].Cells[j].Value);
+                    }
+                }
+
+                string file = exportDialog.FileName;
+                wb.SaveAs(file);
+            }
+
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (string f in files)
+            {
+                if (!File.Exists(f))
+                {
+                    files.Remove(f);
+                    wbs.Remove(f);
+                }
+            }
+            ParseExcels();
+        }
+
+        private OpenFileDialog importDialog = new OpenFileDialog();
+        private SaveFileDialog exportDialog = new SaveFileDialog();
+
+        private List<string> files = new List<string>();
+        private Dictionary<string, XLWorkbook> wbs = new Dictionary<string, XLWorkbook>();
+
+        private void textBoxColumn_TextChanged(object sender, EventArgs e)
+        {
+            Calc(); 
+        }
+
+        private void textBoxRow_TextChanged(object sender, EventArgs e)
+        {
+            Calc();
+        }
+
+        private void textBoxColumn_Validating(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void textBoxColumn_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.KeyChar = char.ToUpper(e.KeyChar);
+            }
+        }
+
+        private void textBoxRow_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("           Cell One 1.0.0              \n \n               For Lisa ", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.weibo.com/hutusi/");
+        }
+    }
+}
